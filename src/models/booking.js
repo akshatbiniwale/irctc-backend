b680b6f2506c4fe3ca7b6d60e2742663e0a502f3
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-05-19 21:06:20 +0530

    added models and route structure

---
+++ src/models/booking.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/db");
+const User = require("./user");
+const Train = require("./train");
+
+const Booking = db.define("Booking", {
+	source: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	destination: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	status: {
+		type: DataTypes.ENUM("booked", "cancelled"),
+		defaultValue: "booked",
+	},
+});
+
+User.hasMany(Booking, { foreignKey: "userId" });
+Booking.belongsTo(User, { foreignKey: "userId" });
+
+Train.hasMany(Booking, { foreignKey: "trainId" });
+Booking.belongsTo(Train, { foreignKey: "trainId" });
+
+module.exports = Booking;
---
+++ src/models/train.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/db");
+
+const Train = db.define("Train", {
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	source: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	destination: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	totalSeats: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	availableSeats: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+});
+
+module.exports = Train;
